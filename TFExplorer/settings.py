532ab4700fc53496bbc8def3fa576fd49737ce39
Adwait Thattey <coderdude1999@gmail.com>
2020-05-07 01:14:46 +0530

    sync: sync with master

--- TFExplorer/settings.py
+++ TFExplorer/settings.py
@@ -120,6 +120,42 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {'()': 'django.utils.log.RequireDebugFalse'},
+        'require_debug_true': {'()': 'django.utils.log.RequireDebugTrue'},
+    },
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler',},
+        'debug_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/main_debug.log',
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 7,
+            #'formatter': 'main_formatter',
+            'filters': ['require_debug_true'],
+        },
+        'null': {"class": 'logging.NullHandler',},
+    },
+    'loggers': {
+        'aemapping': {'handlers': ['console'], 'level': 'DEBUG',},
+        'aenlu': {'handlers': ['console'], 'level': 'DEBUG',},
+        'aekm': {'handlers': ['console'], 'level': 'DEBUG',},
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django': {'handlers': ['null',],},
+        '''py.warnings': {
+            'handlers': ['null', ],
+        },'''
+        '': {'handlers': ['console', 'debug_file'], 'level': "DEBUG",},
+    },
+}
 
 try:
     from .local_settings import *  # noqa: F401,F403
