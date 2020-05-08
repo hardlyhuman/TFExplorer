31c008157007e320e23e7a67110c28690a8a681b
Prahitha <prahitha.movva03@gmail.com>
2020-05-08 20:39:18 +0530

    Updated models.py

--- base/models.py
+++ base/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.db.models import Model
 # Create your models here.
 
+
 class API(Model):
     name = models.CharField('API Name', max_length=256)
     usage = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
     link = models.URLField(max_length=256)
 
+
 class API_Variant(Model):
-    api = models.ForeignKey(API, on_delete = models.CASCADE)
+    api = models.ForeignKey(API, on_delete=models.CASCADE)
     usage = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
